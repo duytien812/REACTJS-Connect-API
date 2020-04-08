a6e8c359644ab3f1484017536d972a76d4c447a7
duytien812 <duytien0812@gmail.com>
2020-04-08 15:35:28 +0700

    87. Connect API : Phân Chia Component 	- Tách giao diện thành các Component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Menu from './components/Menu/Menu';
+import ProductList from './components/ProductList/ProductList';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <div className="navbar navbar-default">
-                    <a className="navbar-brand" href="#">CALL API</a>
-                    <ul className="nav navbar-nav">
-                        <li>
-                            <a>Trang chủ</a>
-                        </li>
-                        <li>
-                            <a>Quản lý sản phẩm</a>
-                        </li>
-                    </ul>
-                </div>
+                <Menu />
 
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            <button type="button" class="btn btn-info mb-10">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <button type="button" className="btn btn-info mb-10">
                                 Thêm sản phẩm
                             </button>
-                            
-                            <div class="panel panel-primary">
-                                  <div class="panel-heading">
-                                        <h3 class="panel-title">Danh Sách Sản Phẩm</h3>
-                                  </div>
-                                  <div class="panel-body">
-                                        <table class="table table-bordered table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th>STT</th>
-                                                    <th>Mã</th>
-                                                    <th>Tên</th>
-                                                    <th>Giá</th>
-                                                    <th>Trạng Thái</th>
-                                                    <th>Hành Động</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>1</td>
-                                                    <td>1</td>
-                                                    <td>Iphone XS Max</td>
-                                                    <td>20.000.000</td>
-                                                    <td>
-                                                        <span className="label label-warning">
-                                                            Còn Hàng
-                                                        </span>
-                                                    </td>
-                                                    <td>
-                                                        <button type="button" class="btn btn-success mr-10">
-                                                            Sửa
-                                                        </button>
-                                                        <button type="button" class="btn btn-danger">
-                                                            Xóa
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                  </div>
-                            </div>
+                            <ProductList />
                         </div>
                     </div>
                 </div>
